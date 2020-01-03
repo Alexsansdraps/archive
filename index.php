d09690a88508063c6ff9757e50760c37de7b52b5
ntoine08 <j.antoine689@laposte.net>
2020-01-03 18:20:41 +0100

    liste personne

--- index.php
+++ index.php
@@ -1,2 +1,19 @@
-<?php
-?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+        <title>ARCHIVE</title>
+    </head>
+
+    <body>
+        <?php
+            include 'connexion.php';   
+        ?>
+        <h1>Archive Dep08</h1>
+        <h2>Ajouter Personne</h2>
+
+    </body>
+
---
+++ personne.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+        <title>ARCHIVE</title>
+    </head>
+
+    <body>
+        
+        <h1>Liste des personnes</h1>
+        <?php
+            include 'connexion.php';  
+             
+            $sel = $bdd->query('SELECT * FROM personne');
+            $personnes=$sel->fetchAll();
+            foreach($personnes as $personne){
+                ?>
+                  <p><?php echo $personne['nomPersonne'];?></p>
+                  <p><?php echo $personne['prenomPersonne'];?></p>
+                  <p><?php echo $personne['adresse'];?></p>
+                  <p><?php echo $personne['mail'];?></p>
+                  <p><?php echo $personne['telephone'];?></p>
+                      
+        <?php
+            };
+        ?>
+        
+        
+        
+
+    </body>
