70ca249dcaf50c47354833973789cd7b2b2b98f1
ntoine08 <j.antoine689@laposte.net>
2020-01-29 19:03:53 +0100

    essai

--- document.php
+++ document.php
@@ -11,7 +11,7 @@
     <body>
     <ul class="index">
         <li><a href="index.php">Accueil</a></li>
-        <li><a href="admin.php">Admin</a></li>
+        <li><a href="admin.php">Admins</a></li>
     </ul>
 
     <table>
