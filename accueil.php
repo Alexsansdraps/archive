6d28887408c4ba5e35c23703bbf98344a7964fa8
ntoine08 <j.antoine689@laposte.net>
2020-02-29 18:29:25 +0100

    29 février

---
+++ accueil.php
@@ -0,0 +1,139 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
+
+    <title>ARCHIVE</title>
+</head>
+
+<body>
+    <?php
+            include 'connexion.php';
+        ?>
+    <ul class="index">
+        <li><a href="index.php">Accueil</a></li>
+        <li><a href="admin.php">Admin</a></li>
+    </ul>
+    <h1>Archive Dep08</h1>
+    <!-- ajouter personne-->
+    <h2>Ajouter Personne</h2>
+
+    <form method="post" action="add-person.php">
+        <table>
+            <tr>
+                <td>nom</td>
+                <td><input type="text" name="nomPersonne" placeholder="Ex : bob"></td>
+            </tr>
+            <tr>
+                <td>Prénom</td>
+                <td><input type="text" name="prenomPersonne" placeholder="Ex : 20 ans"></td>
+            </tr>
+            <tr>
+                <td>Adresse</td>
+                <td><input type="text" name="adresse" placeholder="Ex : 1, rue des aubépines"></td>
+            </tr>
+            <tr>
+                <td>Mail</td>
+                <td><input type="text" name="mail" placeholder="Ex : bob@mail.net"></td>
+            </tr>
+            <tr>
+                <td>Téléphone</td>
+                <td><input type="text" name="telephone" placeholder="Ex :  00 00 00 00 00"></td>
+            </tr>
+        </table>
+        <button type="submit">Envoyer</button>
+    </form>
+
+    <a href="personne.php">Voir</a>
+
+<!-- ajouter document-->
+    <h2>Ajouter Document</h2>
+
+    <form method="post" action="add-doc.php">
+              <p>Sélectionner votre étagères</p>
+              <?php
+                      $sql = "SELECT id_etagere, nomEtagere FROM etagere";
+                      $statement = $bdd->prepare($sql);
+                      $statement->execute();
+                      // var_dump($statement);
+                ?>
+
+                  <select name="doc" id="doc_select">
+                      <?php foreach ($statement as $row) { ?>
+                      <option value=" <?php echo $row['id_etagere']; ?> ">
+                        <?php echo $row['id_etagere']; ?>
+                        étagère nom :
+                        <?php echo $row['nomEtagere']; ?>
+                      </option>
+                      <?php } ?>
+                  </select>
+
+                <p>nom</p>
+
+                <input type="text" name="nomDocument" placeholder="Ex : Normandie">
+                <br><br>
+
+        <button type="submit">Envoyer</button><br><br>
+    </form>
+    <a href="document.php">Voir</a>
+<!-- ajouter zone -->
+    <h2>Ajouter Zone</h2>
+
+    <form method="post" action="add-zone.php">
+        <p>Sélectionner votre lieu de stockage</p>
+            <?php
+                $sqls = "SELECT id_stockage, nomStockage FROM lieustockage";
+                $stockage = $bdd->prepare($sqls);
+                $stockage->execute();
+                      
+            ?>
+
+                  <select name="zone" id="zone_select">
+                      <?php foreach ($stockage as $rows) { ?>
+                      <option value=" <?php echo $rows['id_stockage']; ?> ">
+                        <?php echo $rows['id_stockage']; ?>
+                        Stockage nom :
+                        <?php echo $rows['nomStockage']; ?>
+                      </option>
+                      <?php } ?>
+                  </select>
+        
+                <p>nom</p>
+                <input type="text" name="nomZone" placeholder="Ex : Normandie">
+            
+        <button type="submit">Envoyer</button>
+    </form>
+    <a href="zone.php">Voir</a>
+<!--ajouter etagere-->
+    <h2>Ajouter Etagère</h2>
+
+    <form method="post" action="add-etagere.php">
+    <p>Sélectionner votre zone</p>
+        <?php
+            $sql = "SELECT id_zone, nomZone FROM zone";
+            $zone = $bdd->prepare($sql);
+            $zone->execute();
+                      
+        ?>
+
+                  <select name="etagere" id="etagere_select">
+                      <?php foreach ($zone as $rowz) { ?>
+                      <option value=" <?php echo $rowz['id_zone']; ?> ">
+                        <?php echo $rowz['id_zone']; ?>
+                        Zone nom :
+                        <?php echo $rowz['nomZone']; ?>
+                      </option>
+                      <?php } ?>
+                  </select>
+                <p>nom</p>
+                <input type="text" name="nomEtagere" placeholder="Ex : Normandie">
+            
+        <button type="submit">Envoyer</button>
+    </form>
+    <a href="etagere.php">Voir</a>
+
+</body>
--- connexion.php
+++ connexion.php
@@ -1,7 +1,7 @@
 <?php
     try
     {
-        $bdd = new PDO('mysql:host=localhost; dbname=archive' , 'root', '');
+        $bdd = new PDO('mysql:host=localhost; dbname=archive2' , 'root', '');
 
         // $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         // $bdd->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
